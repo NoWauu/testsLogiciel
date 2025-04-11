0d627dbf0f525fc57f716f9510211c2fa7fe1d16
NoWay_y <teolem77@gmail.com>
2025-04-11 16:26:40 +0200

    Test web avec SpringBoot / MockBean

--- src/test/java/com/example/demo/web/WebTests.java
+++ src/test/java/com/example/demo/web/WebTests.java
@@ -27,4 +27,25 @@ class WebTests {
     @Autowired
     MockMvc mockMvc;
 
+    @Test
+    public void statistique_test() throws Exception {
+        doNothing().when(statistiqueImpl).ajouter(new Voiture("Ferrari", 1_000_000));
+        when(statistiqueImpl.prixMoyen()).thenReturn(new Echantillon(1, 1_000_000));
+        mockMvc.perform(get("/statistique"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nombreDeVoitures").value(1))
+                .andExpect(jsonPath("$.prixMoyen").value(1_000_000))
+                .andReturn();
+    }
+
+    @Test
+    public void voiture_test() throws Exception {
+        mockMvc.perform(post("/voiture")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"marque\": \"Ferrari\", \"prixMoyen\": \"1000000\" }")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
 }

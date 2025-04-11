9ae07854492acdab4ab76a2071bcc307f4e579de
NoWay_y <teolem77@gmail.com>
2025-04-11 14:24:46 +0200

    Tests unitaires classe Voiture

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -4,12 +4,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 public class VoitureTest {
 
     @Test
     void creerVoiture(){
-
+         Voiture voiture = new Voiture("McLaren", 100000);
+         assertEquals("McLaren", voiture.getMarque());
+         assertEquals(100000, voiture.getPrix());
     }
 
 }

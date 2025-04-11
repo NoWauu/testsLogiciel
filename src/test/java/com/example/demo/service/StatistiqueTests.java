97c351a2802dfdcee26d223d097cc1aab7863d21
NoWay_y <teolem77@gmail.com>
2025-04-11 15:33:48 +0200

    Fonctions de test des statistiques avec Mockito

--- src/test/java/com/example/demo/service/StatistiqueTests.java
+++ src/test/java/com/example/demo/service/StatistiqueTests.java
@@ -14,4 +14,27 @@ public class StatistiqueTests {
     @MockBean
     StatistiqueImpl statistiqueImpl;
 
+    @Test
+    public void test_ajouter() {
+        doNothing().when(statistiqueImpl).ajouter(new Voiture());
+    }
+
+    @Test
+    public void test_prix_moyen_voiture_simple() {
+        doNothing().when(statistiqueImpl).ajouter(new Voiture("McLaren", 1_000_000));
+        when(statistiqueImpl.prixMoyen()).thenReturn(new Echantillon(1, 1_000_000));
+    }
+
+    @Test
+    public void test_prix_moyen_multi_voitures() {
+        doNothing().when(statistiqueImpl).ajouter(new Voiture("McLaren", 1_000_000));
+        doNothing().when(statistiqueImpl).ajouter(new Voiture("Ferrari", 500_000));
+        when(statistiqueImpl.prixMoyen()).thenReturn(new Echantillon(2, 750_000));
+    }
+
+    @Test
+    public void test_prix_moyen_exception() {
+        doThrow(ArithmeticException.class).when(statistiqueImpl).prixMoyen();
+    }
+
 }
